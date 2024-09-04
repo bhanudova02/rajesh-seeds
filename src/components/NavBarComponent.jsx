3a37fc2e3ceebcb36484b25900edb6732d3ce180
bhanudova02 <bhanudova02@gmail.com>
2024-09-04 11:11:29 +0530

    new ui changes added

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -1,14 +1,15 @@
-import {Sheet,SheetContent,SheetDescription,SheetHeader,SheetTitle,SheetTrigger,} from "@/components/ui/sheet";
-import { Accordion,AccordionContent,AccordionItem,AccordionTrigger,} from "@/components/ui/accordion";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger, } from "@/components/ui/sheet";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from "@/components/ui/accordion";
 import { AccordionData } from "@/lib/data";
 import { CgMenuRight } from "react-icons/cg";
-
+import { FaTelegram, FaWhatsapp } from "react-icons/fa";
+import { IoCall } from "react-icons/io5";
 export function NavBarComponent() {
     return (
         <nav>
             <Sheet  >
                 <SheetTrigger>
-                    <CgMenuRight className="text-2xl"/>
+                    <CgMenuRight className="text-2xl" />
                 </SheetTrigger>
                 <SheetContent side={"left"}>
                     <img src="/logo.png" alt="Logo" className="w-24" />
@@ -46,6 +47,11 @@ export function NavBarComponent() {
                                 <li className="bg-neutral-900 rounded-md p-2">Careers</li>
                                 <li className="bg-neutral-900 rounded-md p-2">Contact Us</li>
                             </ul>
+                            <div className="bg-neutral-900 rounded-md mt-8 space-y-4 p-4 flex justify-center items-center flex-col">
+                                <button className="flex items-center bg-green-600 py-2 px-8 w-fit gap-1 rounded-full text-sm"><FaWhatsapp className="text-2xl"/> Chat With Us</button>
+                                <button className="flex items-center bg-[#32A9E1] py-2 px-8 w-fit gap-1 rounded-full text-sm"><FaTelegram className="text-2xl"/> Chat With Us</button>
+                                <button className="flex items-center bg-blue-600 py-2 px-8 w-fit gap-1 rounded-full text-sm"><IoCall className="text-2xl"/> Make A Call</button>
+                            </div>
                         </Accordion>
                     </div>
                 </SheetContent>

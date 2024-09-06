8bf136b6b4bd429074681b38ee65dd241b9178df
bhanudova02 <bhanudova02@gmail.com>
2024-09-06 11:41:35 +0530

    new ui changes added

--- src/components/home/OurJourney.jsx
+++ src/components/home/OurJourney.jsx
@@ -4,7 +4,7 @@ export function OurJourney() {
     return (
         <div className="bg-white">
             <div className="w-[90%] md:max-w-7xl mx-auto py-20">
-                <h2 className="mb-8 text-center text-2xl md:text-5xl font-bold bg-gradient-to-r from-sky-900 to-green-600 text-transparent bg-clip-text">
+                <h2 className="mb-8 text-center text-3xl md:text-5xl font-bold bg-gradient-to-r from-sky-900 to-green-600 text-transparent bg-clip-text">
                     Our Journey
                 </h2>
 
@@ -12,14 +12,24 @@ export function OurJourney() {
                     <div className="w-full lg:w-[65%] h-[500px] relative overflow-y-auto shadow-md rounded-md shadow-black custom-scrollbar">
                         <div className="flex justify-center items-center absolute top-4 right-4">
                             <p className="text-lime-900 text-lg animate-bounce font-bold flex items-center mt-2 md:mt-4">
-                                Scroll to view Our Journey <FaAngleDoubleDown className="text-xl" />
+                                Scroll to view Our Journey <FaAngleDoubleDown className="text-lg md:text-xl" />
                             </p>
                         </div>
                         <img src="/Timeline_Journey.png" alt="TimeLine" className="w-[40rem] mx-auto" />
+                       
+                       
                     </div>
                     <div className="relative w-full lg:w-[35%] ">
-                        <img src="/journey_right_box.jpg" alt="journey_right_box.jpg" className="md:absolute top-0 left-0 h-[50vh] md:h-full w-full" />
-                        <div className="absolute inset-0 bg-black bg-opacity-50 p-10 flex justify-center items-center">
+                        {/* <img src="/journey_right_box.jpg" alt="journey_right_box.jpg" className="md:absolute top-0 left-0 h-[50vh] md:h-full w-full" /> */}
+                        <video
+                            src="/Timeline_video.mp4"
+                            autoPlay
+                            muted
+                            loop
+                            playsInline
+                            className="md:absolute top-0 left-0 h-[50vh] object-cover md:h-full w-full"
+                        ></video>
+                        <div className="absolute inset-0 bg-black bg-opacity-70 p-10 flex justify-center items-center">
                             <div>
                                 <h3 className="text-xl md:text-2xl font-semibold mb-4 text-white">Company Overview</h3>
                                 <p className="text-gray-200 mb-4 text-xs md:text-base">
